3862a2249a70cf08a4e71a6ee738f2ab47a9b964
Ilia Romanov <47427592+Iliaromanov@users.noreply.github.com>
2020-08-07 15:22:39 -0400

    Update readingLevelCalculator.c

--- readingLevelCalculator.c
+++ readingLevelCalculator.c
@@ -1,1 +1,27 @@
 #include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    string text = get_string("Text: ");
+}
+
+int countLetters(string text)
+{
+    return 0;
+}
+
+int countWords(string text)
+{
+    return 0;
+}
+
+int countSentences(string text)
+{
+    return 0;
+}
+    
+int calculateGrade(void)
+{
+    return 0;
+}
