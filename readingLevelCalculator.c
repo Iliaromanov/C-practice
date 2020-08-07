a4a81a7c4b4e1a570a652d0a8b0aa20e9dc9a8fb
Ilia Romanov <47427592+Iliaromanov@users.noreply.github.com>
2020-08-07 19:49:38 -0400

    Update readingLevelCalculator.c

--- readingLevelCalculator.c
+++ readingLevelCalculator.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 #include <cs50.h>
 #include <string.h>
+#include <math.h>
 
 int countLetters(string text);
 int countWords(string text);
 int countSentences(string text);
+int calculateGrade(int letters, int words, int sentences);
 
 int main(void)
 {
     string text = get_string("Text: ");
+    int letters = countLetters(text);
+    int words = countWords(text);
+    int sentences = countSentences(text);
+    int grade = calculateGrade(letters, words, sentences);
+    
+    if (grade < 1)
+    {
+        printf("Before Grade 1\n");
+    }
+    else if (grade > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else
+    {
+        printf("Grade %i\n", grade);
+    }
 }
 
 int countLetters(string text)
@@ -54,9 +73,14 @@ int countSentences(string text)
     }
     
     return count;
-}    
+}
     
-int calculateGrade(void)
+int calculateGrade(int letters, int words, int sentences)
 {
-    return 0;
+    float L = letters / (float) words * 100; // Avg number of letters per 100 words
+    float S = sentences / (float) words * 100; // Avg number of words per 100 sentences
+    
+    int grade = round(0.0588 * L - 0.296 * S - 15.8);
+    
+    return grade;
 }
