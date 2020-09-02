703ae16ae475fa92ecb2f57268f0abd2b8f161ce
Ilia Romanov <47427592+Iliaromanov@users.noreply.github.com>
2020-09-02 16:26:55 -0400

    Update recover.c

--- JPEG-recovery/recover.c
+++ JPEG-recovery/recover.c
@@ -1,20 +1,77 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+#define BLOCK_SIZE 512
+
+typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
+    // ensure proper usage
     if (argc != 2)
     {
-        printf("Program should accept exactly one argument\n");
+        printf("Program only takes a single argument\n");
         return 1;
     }
-    
-    // open the file specified with command line argument
-    FILE *forensicImg = fopen(argv[1], 'r');
-    // check that the file can be opened
-    if (*forensicImg == NULL)
+
+    // remember filenames
+    char *infile = argv[1];
+
+    // open input file
+    FILE *inptr = fopen(infile, "r");
+    if (inptr == NULL)
     {
-        printf("Filter could not be opened\n");
-        return 1;
+        printf("Could not open file\n");
+        return 2;
     }
+
+    BYTE buffer[512];
+    int imageCount = 0;
+
+    char filename[8];
+    FILE *outptr = NULL;
+
+    while (true)
+    {
+        // read a block of the memory card image
+        size_t bytesRead = fread(buffer, sizeof(BYTE), BLOCK_SIZE, inptr);
+
+        // break out of the loop when we reach the end of the card image
+        if (feof(inptr))
+        {
+            break;
+        }
+
+        // check if we found a JPEG
+        bool containsJpegHeader = buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0;
+        
+        if (containsJpegHeader)
+        {        
+            // if we found another JPEG, we must close the previous file
+            if (outptr != NULL)
+            {
+                fclose(outptr);
+                imageCount++;
+            }
+            // if we found a JPEG, we need to open the file for writing
+            sprintf(filename, "%03i.jpg", imageCount);
+            outptr = fopen(filename, "w");
+        }
+
+        // write anytime we have an open file
+        if (outptr != NULL)
+        {
+            fwrite(buffer, sizeof(BYTE), bytesRead, outptr);
+        }
+    }
+
+    // close last jpeg file
+    fclose(outptr);
+
+    // close infile
+    fclose(inptr);
+
+    // success
+    return 0;
 }
